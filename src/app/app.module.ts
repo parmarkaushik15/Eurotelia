dcf46459648c15e4abe63b21d41949b15399641f
Kaushik <parmarkaushik15@gmail.com>
2018-07-22 17:47:13 +0530

    Reactive form validation apply

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
@@ -40,9 +41,11 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule,
     NgxIntlTelInputModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { CountryCode } from '../../service/country-code';
 declare var $:any;
+import * as _ from 'google-libphonenumber';
+import { Country } from '../../model/country.model';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +13,14 @@ declare var $:any;
 export class LoginComponent implements OnInit {
   test : Date = new Date();
   
-  private toggleButton;
+    private toggleButton;
     private sidebarVisible: boolean;
     private nativeElement: Node;
-    phone_number:any = "+919376666903";
-    constructor(private element : ElementRef) {
+    selectedCountry: Country = new Country();
+    myForm: FormGroup;
+    private validationMessages: any;
+    allCountries = [];
+    constructor(private element : ElementRef, private formBuilder: FormBuilder) {
         this.nativeElement = element.nativeElement;
         this.sidebarVisible = false;
     }
@@ -36,6 +43,21 @@ export class LoginComponent implements OnInit {
         setTimeout(function(){
             $('.card').removeClass('card-hidden');
         }, 700)
+        this.fetchCountryData();
+
+        this.myForm = this.formBuilder.group({
+            phoneNumber: ['', [Validators.required]],
+            password: ['', [Validators.required]]
+        });
+        this.validationMessages = {
+            'phoneNumber': [
+                {type: 'required', message: 'A Phone number is required.'},
+            ],
+            'password': [
+                {type: 'required', message: 'A Password is required.'},
+            ]
+        };
+
     }
     sidebarToggle(){
         var toggleButton = this.toggleButton;
@@ -54,4 +76,52 @@ export class LoginComponent implements OnInit {
         }
     }
 
+    public onCountrySelect(country: Country, el): void {
+        this.selectedCountry = country;
+    }
+
+    fetchCountryData(): void {
+        CountryCode.allCountries.forEach(c => {
+          let country = new Country();
+          country.name = c[0].toString();
+          country.iso2 = c[1].toString();
+          country.dialCode = c[2].toString();
+          country.priority = +c[3] || 0;
+          country.areaCode = +c[4] || null;
+          country.flagClass = country.iso2.toLocaleLowerCase();
+          country.placeHolder = this.getPhoneNumberPlaceHolder(country.iso2.toUpperCase());
+          this.allCountries.push(country);
+        });
+        this.selectedCountry = this.allCountries[97];
+      }
+      protected getPhoneNumberPlaceHolder(countryCode: string): string {
+        const phoneUtil = _.PhoneNumberUtil.getInstance();
+        const pnf = _.PhoneNumberFormat;
+        try {
+          let phoneNumber = phoneUtil.parse('2236512366', countryCode);
+          return phoneUtil.format(phoneNumber, pnf.INTERNATIONAL);
+        } catch (e) {
+          console.log('CountryCode: "' + countryCode + '" ' + e);
+          return e;
+        }
+      }
+      
+      public onInputKeyPress(event): void {
+        const pattern = /[0-9\+\-\ ]/;
+        let inputChar = String.fromCharCode(event.charCode);
+        if (!pattern.test(inputChar)) {
+          event.preventDefault();
+        }
+      }
+      
+    hasValidationError(validation: any, controlName: (string | number)[]): boolean {
+        const control: AbstractControl = this.myForm.get(controlName);
+        return control.hasError(validation.type) && (control.dirty || control.touched);
+    }
+
+    login(form: FormGroup) {
+        if(form.valid){
+            console.log(form)
+        }
+    }
 }
