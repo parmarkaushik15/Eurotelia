ebf98c36e970647018cd555d26b919665d55dc2d
Kaushik <parmarkaushik15@gmail.com>
2018-07-24 01:06:11 +0530

    Add chart in dashboard

--- src/app/component/pages/dashboard/dashboard.component.ts
+++ src/app/component/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { chart } from 'highcharts';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('outgoingChart') outgoingChart: ElementRef;
+  @ViewChild('incommingChart') incommingChart: ElementRef;
+  outGoingChartOpt: Highcharts.ChartObject;
+  incommingChartOpt: Highcharts.ChartObject;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+    const options: Highcharts.Options = {
+      chart: {
+          type: 'pie',
+          options3d: {
+              enabled: true,
+              alpha: 45
+          }
+      },
+      title: {
+          text: ''
+      },
+      subtitle: {
+          text: ''
+      },
+      plotOptions: {
+          pie: {
+              innerSize: 100,
+              depth: 45
+          }
+      },
+      series: []
+    };
+  
+    this.outGoingChartOpt = chart(this.outgoingChart.nativeElement, options);
+    this.incommingChartOpt = chart(this.incommingChart.nativeElement, options);
+    this.outGoingChartOpt.addSeries({
+        name: 'Country',
+        data: [
+            ['India', 8],
+            ['USA', 3],
+            ['UK', 1],
+            ['France', 6]
+        ]
+    })
+    this.incommingChartOpt.addSeries({
+      name: 'Country',
+      data: [
+          ['India', 8],
+          ['USA', 3],
+          ['UK', 10]
+      ]
+  })  
+  }
+
 
 }
